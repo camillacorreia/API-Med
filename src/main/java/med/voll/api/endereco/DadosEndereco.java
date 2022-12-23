605733bd21a93e27219cbab4f3b884e0c0b3c006
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-12-23 09:56:29 -0300

    feat: add validation

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,11 +1,24 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.*;
+
 public record DadosEndereco(
+  @NotBlank
   String logradouro,
+
+  @NotBlank
   String bairro,
+
+  @NotBlank
+  @Pattern(regexp = "\\d{8}")
   String cep,
+
+  @NotBlank
   String cidade,
+
+  @NotBlank
   String complemento,
+
   String numero,
   String uf) {
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+
+  @NotBlank
+  String nome,
+
+  @NotBlank
+  @Email
+  String email,
+
+  @NotBlank
+  @Pattern(regexp = "\\d{4,6}")
+  String crm,
+
+  @NotNull
+  Especialidade especialidade,
+
+  @NotNull
+  @Valid
+  DadosEndereco endereco) {
 }
