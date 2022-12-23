2e49deaea76cedbcf7306fe74961ce117b808edf
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-12-23 12:30:17 -0300

    feat: att paciente

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -3,6 +3,7 @@ package med.voll.api.paciente;
 import jakarta.persistence.*;
 import lombok.*;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
@@ -30,5 +31,17 @@ public class Paciente {
     this.telefone = dados.telefone();
     this.cpf = dados.cpf();
     this.endereco = new Endereco(dados.endereco());
+  }
+
+  public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+    if (dados.nome() != null) {
+      this.nome = dados.nome();
+    }
+    if (dados.telefone() != null) {
+      this.telefone = dados.telefone();
+    }
+    if (dados.endereco() != null) {
+      this.endereco.atualizarInformacoes(dados.endereco());
+    }
   }
 }
