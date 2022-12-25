301702b9df153d41d701240f00323d6a6121eb18
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-12-25 13:01:51 -0300

    feat: authenticating the user

--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,9 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authorizeHttpRequests()
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated()
             .and().build();
   }
 
