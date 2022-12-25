1a6f1188f03cb7886d778f6bb5480139a4315969
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-12-25 12:29:50 -0300

    feat: retrieving the token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -14,7 +14,16 @@ public class SecurityFilter extends OncePerRequestFilter {
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    var tokenJWT = recuperarToken(request);
     filterChain.doFilter(request, response);
+  }
+
+  private String recuperarToken(HttpServletRequest request) {
+    var authorizationHeader = request.getHeader("Authorization");
+    if (authorizationHeader == null) {
+      throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+    }
+
+    return authorizationHeader.replace("Bearer ", "");
   }
 }
